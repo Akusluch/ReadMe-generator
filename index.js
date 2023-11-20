c5156926fbc7dc5cbe7ec568ea341236df309d60
Alphonse Kusluch <alkusluch@gmail.com>
2023-11-20 17:44:21 -0500

    link files together properly

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        fileName: 'project name',
+        message: 'What is the name of your project?'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then(writeToFile(fileName, data))
+}
 
 // Function call to initialize app
 init();
