29c5508e9664da351401886f15afa3db4add6f4b
Alphonse Kusluch <alkusluch@gmail.com>
2023-11-21 15:25:53 -0500

    add quetions and alot of the functionallity to generate the data into proper readme file structure as well as print data to a new file

--- index.js
+++ index.js
@@ -6,19 +6,61 @@ const generateMarkdown = require('./utils/generateMarkdown');
 const questions = [
     {
         type: 'input',
-        fileName: 'project name',
+        name: 'title',
         message: 'What is the name of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description of your project?'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'what are your installation instructions?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the use of your project?'
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'What are your contribution guidlines?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What are your test instructions?'
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your Github Username?'
     }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, error => {
+        if (error){
+            console.log(error)
+        }
+    })
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer
-        .prompt(questions)
-        .then(writeToFile(fileName, data))
+    inquirer.prompt(questions)
+        .then (answers => {
+            console.log(answers);
+            return generateMarkdown(answers)
+        })
+        .then (pageMarkdown => {
+            writeToFile('./test/README.md', pageMarkdown);
+            console.log('README.md created')
+        })
 }
 
 // Function call to initialize app
---
+++ test/README.md
@@ -0,0 +1,15 @@
+# AK
+  ## Description
+  TBD
+  ## Installation
+  TBD
+  ## Usage
+  TBD
+  ## License
+  undefined
+  ## Contribute
+  TBD
+  ## Tests
+  undefined 
+  ##Questions
+  [Link to my GitHub Profile](https://github.com/Akusluch)
